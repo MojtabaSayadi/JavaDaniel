6599a934ebf41f6e27cd6057c9a7a74f7d76a96a
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2024-03-26 18:19:14 +0430

    14030107

---
+++ chapter5/Problem$37.java
@@ -0,0 +1,32 @@
+package chapter_five;
+
+import java.util.Scanner;
+
+/**
+ * **5.37 (Decimal to binary) Write a program that prompts the user to enter a decimal
+ * integer then displays its corresponding binary value. Don’t use Java’s Integer
+ * .toBinaryString(int) in this program.
+ *
+ * @  Mojtaba Sayadi
+ * */
+
+public class Problem$37
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("enter a decimal value: ");
+        int number = scanner.nextInt();
+
+        StringBuilder binary = new StringBuilder();
+        while (number > 0)
+        {
+            binary.append(number % 2);
+            number /= 2;
+        }
+
+        System.out.println(binary.reverse());
+    }
+}
+
---
+++ chapter5/Problem$49.java
@@ -0,0 +1,44 @@
+package chapter_five;
+
+import java.util.Scanner;
+
+/**
+ * *5.49 (Count vowels and consonants) Assume that the letters A, E, I, O, and U are vowels.
+ * Write a program that prompts the user to enter a string, and displays the number of
+ * vowels and consonants in the string.
+ *
+ *      Enter a string: Programming is fun
+ *      The number of vowels is 5
+ *      The number of consonants is 11
+ *
+ * @  Mojtaba Sayadi
+ * */
+
+
+public class Problem$49
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String string = scanner.nextLine();
+
+        int vowelsOccurrence = 0;
+        int consonantsOccurrence = 0;
+
+        for (int i = 0; i < string.length(); i++)
+        {
+            char x = Character.toLowerCase(string.charAt(i));
+            if ( x =='a' || x == 'e' || x == 'i'
+                    || x == 'o' || x == 'u')
+                vowelsOccurrence++;
+            else if (Character.isLetter(x))
+                consonantsOccurrence++;
+        }
+
+        System.out.println("The number of vowels is " + vowelsOccurrence);
+        System.out.println("The number of consonants " + consonantsOccurrence);
+    }
+}
+
