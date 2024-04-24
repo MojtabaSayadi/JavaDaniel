3d7d76e3ca85cbb15dda75f74d4c3d642628b6ec
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2024-04-24 21:26:53 +0430

    14030205

---
+++ chapter11/Problem$10.java
@@ -0,0 +1,28 @@
+package chapter_eleven;
+
+import java.util.Scanner;
+
+/**
+ * 11.10 (Implement MyStack using inheritance) In Listing 11.10, MyStack is
+ * implemented using composition. Define a new stack class that extends ArrayList.
+ * Draw the UML diagram for the classes then implement MyStack. Write a test
+ * program that prompts the user to enter five strings and displays them in reverse order
+ *
+ * @ Mojtaba Sayadi
+ * */
+public class Problem$10
+{
+    public static void main(String[] args)
+    {
+        MyStack<String> myStack = new MyStack<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("enter strings: ");
+        for (int i = 0; i < 5;i++)
+            myStack.push(scanner.next());
+
+        myStack.reverse();
+        System.out.println(myStack);
+    }
+}
