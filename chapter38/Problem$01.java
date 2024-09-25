9597149503d2d0460de765631ccc25a7e3a897a7
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2024-09-25 20:44:05 +0330

    14030704

---
+++ chapter38/Problem$01.java
@@ -0,0 +1,11 @@
+package chapter38;
+
+/**
+ * 38.1 (Factorial table in JSP) Rewrite Exercise 37.1 using JSP.
+ *
+ * @ Mojtaba Sayadi
+ * */
+public class Problem$01
+{
+    // see Problem$01.jsp
+}
