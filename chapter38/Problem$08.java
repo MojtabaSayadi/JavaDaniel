41cfed4910ee7955fe30c17e081c17af6d8c69ad
MojtabaSayadi <141604831+MojtabaSayadi@users.noreply.github.com>
2024-10-03 18:41:58 +0330

    14030712

---
+++ chapter38/Problem$08.java
@@ -0,0 +1,12 @@
+package chapter38;
+
+/**
+ * *38.8 (Store cookies in JSP) Rewrite Exercise 37.10 using JSP. Use response.
+ * addCookie(Cookie) to add a cookie.
+ *
+ * @ Mojtaba Sayadi
+ * */
+public class Problem$08
+{
+    // see Problem$08.jsp
+}
